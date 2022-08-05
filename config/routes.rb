b481f74a326e974ad851e2a58e5bc0156c1ebdfb
Aditya Singh <saditya370@gmail.com>
2022-08-05 18:21:33 +0530

    Added Comment/Delete options

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   root "articles#index"
   # get "/articles", to: "articles#index"
   # delte "/articles/:id", to: "articles#destroy"
-  resources :articles
+  resources :articles do
+      resources :comments
+  end
 end
